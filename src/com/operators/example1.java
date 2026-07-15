33af7db73f8510b034f8c043363a02256a7e70e2
vadlagayathri26 <vadlagayathri011@gmail.com>
2026-07-15 19:08:39 +0530

    java

---
+++ src/com/operators/example1.java
@@ -0,0 +1,18 @@
+package com.operators;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		int a=10;
+	
+		System.out.println("Enter Number : " + a);
+		System.out.println("Original Number :"+ a);
+		System.out.println("Complement Result:"+ (~a));
+		
+	
+	
+	
+	
+	}
+
+}
