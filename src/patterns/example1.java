8b721474e1182a4b15eadb13cf4a85aad770654b
vadlagayathri26 <vadlagayathri011@gmail.com>
2026-07-24 10:25:13 +0530

    patterns

---
+++ src/patterns/example1.java
@@ -0,0 +1,18 @@
+package patterns;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		
+		int num=50;
+		int fact =0;
+		for(int i=1;i<num;i++) {
+			fact =fact*i;
+		}
+		
+	}	
+}
+
+
+
+
