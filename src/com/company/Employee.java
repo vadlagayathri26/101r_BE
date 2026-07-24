a345ae9726161421a20b98928e50e14112e68f62
vadlagayathri26 <vadlagayathri011@gmail.com>
2026-07-24 11:25:51 +0530

    employee

---
+++ src/com/company/Employee.java
@@ -0,0 +1,12 @@
+package com.company;
+import com.employee.employee;
+
+public class Employee {
+
+	public static void main(String[] args) {
+
+		employee emp =new employee();
+		emp.setDetails();
+		emp.displayDetails();
+	}
+}
