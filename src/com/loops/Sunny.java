e8dad14b692dec724a9e25bdfe279a515af19804
vadlagayathri26 <vadlagayathri011@gmail.com>
2026-07-24 10:10:25 +0530

    add

---
+++ src/com/loops/Sunny.java
@@ -0,0 +1,20 @@
+package com.loops;
+
+public class Sunny {
+
+	public static void main(String[] args) {
+int num=8;
+int next=num+1;
+int root=(int)Math.sqrt(next);
+
+		if(root*root==next) {
+			System.out.println("sunny number");
+			
+		}else {
+			System.out.println(" not sunny number ");
+		}
+		
+		
+	}
+
+}
